f9492c6e77162752d5e7b46b88705fdbfbdfc004
Mehdi-Adel <zeadel.mehdi@gmail.com>
2020-08-25 02:35:25 +0200

    skills category + branded menu only for home

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,33 +1,33 @@
 // Sélection des objets du DOM
 
-const menuBtn = document.querySelector(".menu-btn");
-const menu = document.querySelector(".menu");
-const menuNav = document.querySelector(".menu-nav");
-const menuBranding = document.querySelector(".menu-branding");
-const navItems = document.querySelectorAll(".nav-item");
+const menuBtn = document.querySelector('.menu-btn');
+const menu = document.querySelector('.menu');
+const menuNav = document.querySelector('.menu-nav');
+const menuBranding = document.querySelector('.menu-branding');
+const navItems = document.querySelectorAll('.nav-item');
 
 // Configuration initiale du menu
 
 let showMenu = false;
 
-menuBtn.addEventListener("click", toggleMenu);
+menuBtn.addEventListener('click', toggleMenu);
 
 function toggleMenu() {
-  if (!showMenu) {
-    menuBtn.classList.add("close");
-    menu.classList.add("show");
-    menuNav.classList.add("show");
-    menuBranding.classList.add("show");
-    navItems.forEach((item) => item.classList.add("show"));
-    // Configuration de l'état du menu
-    showMenu = true;
-  } else {
-    menuBtn.classList.remove("close");
-    menu.classList.remove("show");
-    menuNav.classList.remove("show");
-    menuBranding.classList.remove("show");
-    navItems.forEach((item) => item.classList.remove("show"));
-    // Configuration de l'état du menu
-    showMenu = false;
-  }
+    if (!showMenu) {
+        menuBtn.classList.add('close');
+        menu.classList.add('show');
+        menuNav.classList.add('show');
+        menuBranding.classList.add('show');
+        navItems.forEach((item) => item.classList.add('show'));
+        // Configuration de l'état du menu
+        showMenu = true;
+    } else {
+        menuBtn.classList.remove('close');
+        menu.classList.remove('show');
+        menuNav.classList.remove('show');
+        menuBranding.classList.remove('show');
+        navItems.forEach((item) => item.classList.remove('show'));
+        // Configuration de l'état du menu
+        showMenu = false;
+    }
 }
